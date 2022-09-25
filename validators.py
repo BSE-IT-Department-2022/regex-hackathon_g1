574c9c7c2290c3a468d87ef951d17a7af7877720
Adura-Hephzibah <adurahephzibah@gmail.com>
2022-09-26 00:03:36 +0200

    more validator functions

--- validators.py
+++ validators.py
@@ -15,3 +15,44 @@ def validate_birthdate(date):
         print('Your birthdate is NOT valid')
     else:
         print('Your birthdate is valid')
+
+
+def validate_username(fullname, username):
+    """Validate a username.
+    Args:
+         fullname (str): the validated fullname of the user
+         username (str): the username to validate from the fullname
+    """
+
+
+    first_name, middle_name, last_name = re.split(' |/|\.', fullname)
+
+
+    # a username is the form  <Firstname><Lastname>
+    username_match = re.fullmatch(r'{}{}'.format(first_name, last_name), username)
+
+
+    if not username_match:
+        print('Your username is NOT valid!')
+    else:
+        print('Your username is valid')
+
+
+def validate_email(fullname, email):
+    """Validate an email address of a user.
+    Args:
+         fullname (str): the validated fullname of the user
+         email (str): the email to validate from the fullname
+    """
+    # extract the firstname and lastname from the fullname
+    first_name, middle_name, last_name = re.split(' |/|\.', fullname)
+
+    # email is in the form <f>.<last>@<org_name>.email where <f> is the first letter of the first name
+    email_pattern = re.compile(rf'^{first_name[0].lower()}\.{last_name.lower()}@[a-z]*\.email$')
+
+    email_match = re.fullmatch(email_pattern, email)
+
+    if not email_match:
+        print('Your email is NOT valid!')
+    else:
+        print('Your email is valid')
